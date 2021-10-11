a3d501638e0dbb98682f094e4085fab361399072
sanghyuki83 <sanghyuki83@gmail.com>
2021-10-11 18:28:32 +0900

    feat: 게임 시작 호출 반영

---
+++ src/main/java/racinggame/RacingGame.java
@@ -0,0 +1,13 @@
+package racinggame;
+
+public class RacingGame {
+    private static final RacingGame game = new RacingGame();
+
+    private RacingGame() {}
+
+    public static RacingGame getInstance() { return game; }
+
+    public void start(){
+
+    }
+}
